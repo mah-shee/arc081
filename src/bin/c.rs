6a5492f844c7bfe6c795e955ef785ece22f8bd7c
鎌田正聖 <masatoshi@masa-macbook-pro.local>
2020-07-20 19:51:40 +0900

    Done Cargo.

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -4,5 +4,29 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        mut a: [usize; n]
     }
+    let mut pair = vec![0; n];
+    a.sort();
+    let mut i = n;
+    let mut skip = false;
+    while i > 1 {
+        if skip {
+            skip = false;
+            i -= 1;
+            continue;
+        }
+        if a[i - 1] == a[i - 2] {
+            if pair[0] == 0 {
+                pair[0] = a[i - 2];
+            } else {
+                pair[1] = a[i - 2];
+                break;
+            }
+            skip = true;
+        }
+        i -= 1;
+    }
+    println!("{}", pair[0] * pair[1]);
 }
